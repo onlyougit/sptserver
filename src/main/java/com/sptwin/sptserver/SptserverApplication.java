ba7600cd3f4f7ce2a75af79a421d2f8bddd31ce7
wangweibin <aliding123@dingtalk.com>
2018-03-14 17:44:41 +0800

    测试

--- src/main/java/com/sptwin/sptserver/SptserverApplication.java
+++ src/main/java/com/sptwin/sptserver/SptserverApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableCaching
 @EnableSwagger2
+//@EnableEurekaServer
 @MapperScan("com.sptwin.sptserver.base.mapper")
 //@ImportResource({"classpath:mybatis-config.xml","classpath:mybatis-config.xml"})
 public class SptserverApplication {
