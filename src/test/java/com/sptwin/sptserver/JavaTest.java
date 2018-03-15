f95e130fb0adbe29d9c3ceaffb8fbfb61b229f38
wangweibin <aliding123@dingtalk.com>
2018-03-15 17:40:12 +0800

    测试

---
+++ src/test/java/com/sptwin/sptserver/JavaTest.java
@@ -0,0 +1,9 @@
+package com.sptwin.sptserver;
+
+public class JavaTest {
+    public static void main(String[] args) {
+        Integer a = 129;
+        int b = 129;
+        System.out.println(a==b);
+    }
+}
